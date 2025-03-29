775a79ad68e7cb415ed9efd1b08a20a23e124443
yassine <yassinebnyahia@gmail.com>
2025-03-29 03:29:36 +0100

    class pokemon : getters setters et attack method

--- Heritage-Polymorphisme/Pokemon.php
+++ Heritage-Polymorphisme/Pokemon.php
@@ -7,11 +7,26 @@ class Pokemon{
         protected int $hp,
         protected attackPokemon $attack,
     )
-    {
-        
-    }
-    
+    {}
+    public function getNom(){return $this->nom;}
+    public function getURL(){return $this->url;}
+    public function getHp(){return $this->hp;}
+    public function getAttack(){return $this->attack;}
+    public function setNom(string $n){$this->nom=$n;}
+    public function setURL(string $n){$this->url=$n;}
+    public function setAttack(attackPokemon $n){$this->attack=$n;}
+    public function setHp(int $n){$this->hp=$n;}
 
+    public function attack(Pokemon $p){
+        if (mt_rand(1,100)<=$this->attack->probabilitySpecialAttack){
+            $damage=($this->attack->specialAttack)*(mt_rand($this->attack->attackMinimal,$this->attack->attackMaximal));
+        }
+        else{
+            $damage=mt_rand($this->attack->attackMinimal,$this->attack->attackMaximal);
+        }
+        $oldHp=$p->getHp();
+        $p->setHp($oldHp-$damage);
+    }
     
 }
 
