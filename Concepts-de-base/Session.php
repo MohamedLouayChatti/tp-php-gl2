9310cb0bb2be63dffb423933f4d522c13aeaad35
MohamedLouayChatti <louaychatti11@gmail.com>
2025-04-02 16:40:39 +0100

    Finishing session class

---
+++ Concepts-de-base/Session.php
@@ -0,0 +1,40 @@
+<?php
+
+class Session{
+    private static $sessionData;
+    private static $visits;
+
+    public static function init(){
+        if ($_SERVER['REQUEST_URI'] === '/favicon.ico') {
+            //En cas de 2 requetes
+            return;
+        }
+        if(isset($_COOKIE["nbVisits"])){
+            self::$visits = $_COOKIE["nbVisits"];
+            self::$visits += 1; 
+            setcookie("nbVisits", self::$visits);
+        }else{
+            self::$visits = 1;
+            setcookie("nbVisits", self::$visits);
+        }
+        foreach($_COOKIE as $key => $val){
+            self::$sessionData[$key] = $val;
+        }
+
+    }
+
+    public static function addData($key, $val){
+        self::$sessionData[$key] = $val;
+        setcookie($key, $val);
+    }
+    
+    public static function getData($key){
+        return self::$sessionData[$key];
+    }
+
+    public static function getVisits(){
+        return self::$visits;
+    }
+}
+
+?>
