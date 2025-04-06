f3d6919e512111a5680418cbc8b07f65b2ec5bfa
Your Name <Mohamed Amine LAOUINI>
2025-04-06 22:20:10 +0100

    Edited new table etudiant

--- PDO/database/etudiants.sql
+++ PDO/database/etudiants.sql
@@ -1,69 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 05, 2025 at 05:25 PM
--- Server version: 8.0.41
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `etudiants`.`etudiant` 
+(`id` INT NOT NULL AUTO_INCREMENT ,
+ `name` VARCHAR(25) NOT NULL ,
+ `dateDeNaissance` DATE NOT NULL , 
+ `section` ENUM('GL','RT','IMI','IIA','MPI','CBA','BIO','CH') NOT NULL , 
+ `imageURL` VARCHAR(100) NOT NULL DEFAULT 'img/default.png' , PRIMARY KEY (`id`)) 
+ ENGINE = InnoDB DEFAULT CHARBSBQSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Database: `etudiants`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `etudiants`
---
-
-CREATE TABLE `etudiants` (
-  `id` int NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `dateDeNaissance` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `etudiants`
---
-
-INSERT INTO `etudiants` (`id`, `name`, `dateDeNaissance`) VALUES
-(1, 'yassine benyahia', '2025-04-02'),
-(2, 'louay chatti', '2005-04-30'),
-(3, 'med amine laaouini', '2004-04-01'),
-(4, 'sadri skander', '2024-10-01');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `etudiants`
---
-ALTER TABLE `etudiants`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `etudiants`
---
-ALTER TABLE `etudiants`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ INSERT INTO `etudiant` (`id`, `name`, `dateDeNaissance`, `section`, `imageURL`) VALUES
+  ('1', 'yassine benyahia', '2025-04-02', 'GL', 'img/default.png'),
+   ('2', 'louay chatti', '2005-04-30', 'GL', 'img/louaychatti.png'),
+    ('3', 'med amine laouini', '2004-04-01', 'GL', 'img/medaminelaouini.png'),
+  ('4', 'sadri skander', '2024-10-01', 'IMI', 'img/sadriskander.png');
